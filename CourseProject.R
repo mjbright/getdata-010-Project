c23404bc12c2e73f8494755f9b8ef3b45ce26936
Michael Bright <mjbright@github.com>
2015-01-25 22:48:54 +0100

    2nd commit - added comments into R project code

--- CourseProject.R
+++ CourseProject.R
@@ -1,26 +1,30 @@
 
+# Install needed packages:
+#
 #install.packages("data.table")
 #install.packages("reshape2")
-
 #install.packages("knitr")
 
 library(data.table)
 library("reshape2")
 library("knitr")
 
+# Change to project directory:
 setwd("D:/DATA/GettingCleaningData/Project")
 
-
+# Read in subject training/test data:
 dtTrain <- fread("Dataset/train/subject_train.txt")
 dtTest  <- fread("Dataset/test/subject_test.txt" )
 
+# Read in activity training/test data:
+
+# Helper function to read in large data sets correctly:
 readtable <- function (file) {
   dataf <- read.table(file)
   dt <- data.table(dataf)
   
   return(dt)
 }
-
 
 dtATrain <- readtable("Dataset/train/Y_train.txt")
 dtATest <- readtable("Dataset/test/Y_test.txt")
@@ -29,8 +33,10 @@ dtTest <- readtable("Dataset/test/X_test.txt")
 
 # Merge training and test datasets::
 dtSubject <- rbind(dtTrain, dtTest)
-setnames(dtSubject, "V1", "subject")
 dtActivity <- rbind(dtATrain, dtATest)
+
+# Set column names:
+setnames(dtSubject, "V1", "subject")
 setnames(dtActivity, "V1", "activityNum")
 
 dt <- rbind(dtTrain, dtTest)
@@ -66,27 +72,28 @@ dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by="fe
 dt$activity <- factor(dt$activityName)
 dt$feature <- factor(dt$featureName)
 
-grepthis <- function (regex) {
+grep_feature <- function (regex) {
   grepl(regex, dt$feature)
 }
+
 ## Features with 2 categories
 n <- 2
 y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grepthis("^t"), grepthis("^f")), ncol=nrow(y))
+x <- matrix(c(grep_feature("^t"), grep_feature("^f")), ncol=nrow(y))
 dt$featDomain <- factor(x %*% y, labels=c("Time", "Freq"))
-x <- matrix(c(grepthis("Acc"), grepthis("Gyro")), ncol=nrow(y))
+x <- matrix(c(grep_feature("Acc"), grep_feature("Gyro")), ncol=nrow(y))
 dt$featInstrument <- factor(x %*% y, labels=c("Accelerometer", "Gyroscope"))
-x <- matrix(c(grepthis("BodyAcc"), grepthis("GravityAcc")), ncol=nrow(y))
+x <- matrix(c(grep_feature("BodyAcc"), grep_feature("GravityAcc")), ncol=nrow(y))
 dt$featAcceleration <- factor(x %*% y, labels=c(NA, "Body", "Gravity"))
-x <- matrix(c(grepthis("mean()"), grepthis("std()")), ncol=nrow(y))
+x <- matrix(c(grep_feature("mean()"), grep_feature("std()")), ncol=nrow(y))
 dt$featVariable <- factor(x %*% y, labels=c("Mean", "SD"))
 ## Features with 1 category
-dt$featJerk <- factor(grepthis("Jerk"), labels=c(NA, "Jerk"))
-dt$featMagnitude <- factor(grepthis("Mag"), labels=c(NA, "Magnitude"))
+dt$featJerk <- factor(grep_feature("Jerk"), labels=c(NA, "Jerk"))
+dt$featMagnitude <- factor(grep_feature("Mag"), labels=c(NA, "Magnitude"))
 ## Features with 3 categories
 n <- 3
 y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grepthis("-X"), grepthis("-Y"), grepthis("-Z")), ncol=nrow(y))
+x <- matrix(c(grep_feature("-X"), grep_feature("-Y"), grep_feature("-Z")), ncol=nrow(y))
 dt$featAxis <- factor(x %*% y, labels=c(NA, "X", "Y", "Z"))
 
 r1 <- nrow(dt[, .N, by=c("feature")])
